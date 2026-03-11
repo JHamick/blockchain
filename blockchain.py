efb7fb846d6e54b52187658e384dbb021b8df498
JHamick <jackhamick2004@gmail.com>
2026-03-11 20:26:55 +0100

    Added proof of work functionality to the blockchain.

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+
+from flask import Flask
 
 class blockchain(object):
     def __init__(self):
@@ -65,4 +69,36 @@ class blockchain(object):
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        return self.chain[-1]
+        return self.chain[-1] 
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple proof of work alogrithm:
+        Find a number p' such that hash(pp') contains leading 4 zeros where p is the previous p'
+        p is the previous proof, and p' is the new proof
+        Args:
+            last_proof (int)
+            return (int)
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof +=1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the proof: Does has(last_proof, proof) contain 4 leading zeros?
+        Args:
+            last_proof (int) Previous proof
+            proof (int) Current proof
+            return (bool) True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hast = hashlib.sha256(guess).hexdigest()
+        return guess_has[:4] = "0000"
+
+    
